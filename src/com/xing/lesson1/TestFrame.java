cf12f6efc6d94a516f03fa2aeae374a9b70fd201
xfh <2030280200@qq.com>
2020-11-24 23:08:24 +0800

    Java回顾知识点复习巩固

---
+++ src/com/xing/lesson1/TestFrame.java
@@ -0,0 +1,14 @@
+package com.xing.lesson1;
+
+import java.awt.*;
+
+//gui的第一个界面
+public class TestFrame {
+    public static void main(String[] args) {
+        //Frame ，JDK 看源码
+        Frame frame = new Frame ("我的第一个Java图形界面窗户");
+
+        //需要设置可见行
+
+    }
+}
